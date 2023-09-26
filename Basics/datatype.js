709bd128ab1c9c7c93891bf7a269f8d7ac573a97
a2sharma97 <142878177+a2sharma97@users.noreply.github.com>
2023-09-26 14:53:52 +0000

    datatype

---
+++ Basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"
+// console.log(3+3);
+// console.log("Abhishek");
+
+let name="Abhishek"
+let age=25
+let isLoggedIn=true
+let state;
+let city=null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof state);
+console.log(typeof city);
+console.log(typeof undefined);
+console.log(typeof sdgfsdg);
