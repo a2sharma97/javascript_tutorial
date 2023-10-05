f4aafc31a1b1fbb1dc0c0f3af39d6754f85d52ea
a2sharma97 <142878177+a2sharma97@users.noreply.github.com>
2023-10-05 02:04:15 +0000

    oerations and  complex conversion

---
+++ Basics/operations.js
@@ -0,0 +1,47 @@
+let value=31
+let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3**2);
+// console.log(4/2);
+// console.log(5%2);
+
+let str1="Hellio"
+let str2=" Abhishek"
+// console.log(str1+str2);
+
+// console.log(typeof("1" + 2)); //if fistrt operand is string then the whole expression is convert into string
+// console.log(typeof(1+"2")); //also convert into string
+// console.log(typeof("1" + 2 + 2));//also convert into string
+// console.log(typeof("1" + "2" + 2));//also convert into string
+// console.log(typeof(1 + "2" + 2));//also convert into string
+// console.log(typeof(1 + "2" + "2"));//also convert into string
+// console.log(typeof("1" + 2 + "2"));//also convert into string
+// console.log(typeof(1 + 2 + "2"));//also convert into string
+
+// console.log("1" + 2); //12
+// console.log(1+"2"); //12
+// console.log("1" + 2 + 2);//122
+// console.log("1" + "2" + 2);//122
+// console.log(1 + "2" + 2);//122
+// console.log(1 + "2" + "2");//122
+// console.log("1" + 2 + "2");//122
+// console.log(1 + 2 + "2");//32
+
+// console.log(true);
+// console.log(+true);//1
+// console.log(typeof(+true));//convert boolean into number
+// // console.log(true+);//error
+
+// console.log(" ");//nothing is print
+// console.log(+"");//0
+// console.log(typeof(+""));//convert into number
+
+let counter=100
+counter++;//postfix
+console.log(counter);
+
+++counter;//prefix
+console.log(counter);
