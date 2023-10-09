59f93116f90daf9025dd1a3664fe427167f57277
a2sharma97 <142878177+a2sharma97@users.noreply.github.com>
2023-10-09 00:58:12 +0000

    comparisons and strict check

---
+++ Basics/comparison.js
@@ -0,0 +1,35 @@
+"use strict"
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//differnt datatype comparisons
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log("0">1);
+// console.log("null">1);
+// console.log("ab">1);
+
+// console.log(null>0);//comparison operator works differntly in js, converts null to 0
+// console.log(null<0);//comparison operator works differntly in js
+// console.log(null<=0);//comparison operator works differntly in js
+// console.log(null==0);//equality operator works differently in js, not convert
+// console.log(null>=0);//comparison operator works differntly in js
+//  console.log(null!=0);//comparison operator works differntly in js ,not convert
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
+// console.log(undefined!=0);
+
+/*STRICT CHECk */ //check both values and datatype
+console.log("2"==2);
+console.log("2"===2);//strict check
+console.log("2"!==2);//strict check
+
