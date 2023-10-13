03e614aa0928c0b45541adad4f7c8c1d76b9ef81
a2sharma97 <142878177+a2sharma97@users.noreply.github.com>
2023-10-13 01:46:56 +0000

    strings

---
+++ Basics/strings.js
@@ -0,0 +1,23 @@
+"use strict"
+const name="Abhishek"
+const repo=50
+//console.log(name + repo + " value");
+// console.log(`hello my name is ${name} and my repo count is ${repo}`);//industry way to print something(use ` not single or double quote)
+
+//Decleration of string as object
+const gameName=new String('AbhishekAK');
+//console.log(typeof name); //string
+//console.log(typeof gameName);//object
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(9));
+// console.log(gameName.indexOf('k'));
+
+ // substring
+const playerName=new String('Abhishek-AK');
+const newString=playerName.substring(1,4);
+console.log(newString);
+
+// slicing
