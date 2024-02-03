52e9ef8dc152b8be9141551e33dca665b0384ae8
a2sharma97 <142878177+a2sharma97@users.noreply.github.com>
2024-02-03 18:45:12 +0000

    Math_Object

---
+++ Basics/math.js
@@ -0,0 +1,21 @@
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(23.2))
+// console.log(Math.round(23.5))
+// console.log(Math.round(23.9))
+// console.log(Math.ceil(8.5))
+// console.log(Math.floor(8.5))
+// console.log(Math.min(2,5,1,6,8))
+// console.log(Math.max(2,5,1,6,8))
+// console.log(Math.PI)
+console.log(Math.random())/*y always ek random number deta h jo 0 and 1 k bich hota h*/
+/*if we want that k hmara number 0 s bda aaye to hum 10 s multiply kr skte h*/
+console.log(Math.random()*10);
+/*if we want that k only single digit number h aaye to hm floor fuction ka use kr skte h */
+console.log(Math.floor(Math.random()*10));
+/*if we want that k only single digit number h aaye but 0 na aaye to hm floor fuction k sath +1 ka use krte h*/
+console.log(Math.floor(Math.random()*10)+1);
+/*if we want k number ek particular range m aaye min=10 and max=20 console.log(Math.floor(Math.random()*(max-min+1))+min);*/
+console.log(Math.floor(Math.random()*(20-10+1))+10);
+/*Range of dice is 1 to 6 */
+console.log(Math.floor(Math.random()*(6-1+1))+1);
